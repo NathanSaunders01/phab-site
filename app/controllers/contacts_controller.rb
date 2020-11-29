b0f9c63131e5d30040f748dd6fef2bd5eeac39c6
Nathan Saunders <nathan_saunders@hotmail.co.uk>
2020-11-29 17:50:27 +0000

    Remove sendgrid gem

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,3 @@
-require 'sendgrid-ruby'
-include SendGrid
-
 class ContactsController < ApplicationController
 
   def new 
@@ -8,25 +5,14 @@ class ContactsController < ApplicationController
   end
 
   def create
-    # @contact = Contact.new(params[:contact])
-    # @contact.request = request
-    from = Email.new(email: 'test@example.com')
-    to = Email.new(email: 'test@example.com')
-    subject = 'Sending with SendGrid is Fun'
-    content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
-    mail = Mail.new(from, subject, to, content)
-
-    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-    response = sg.client.mail._('send').post(request_body: mail.to_json)
-    puts response.status_code
-    puts response.body
-    puts response.headers
-    # if @contact.deliver
-    #   flash[:success] = 'Your message sent successfully!'
-    #   redirect_to root_path
-    # else
-    #   flash[:error] = 'There was a problem sending your message.'
-    #   redirect_to contact_us_path
-    # end
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      flash[:success] = 'Your message sent successfully!'
+      redirect_to root_path
+    else
+      flash[:error] = 'There was a problem sending your message.'
+      redirect_to contact_us_path
+    end
   end
 end
